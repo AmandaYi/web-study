991d1a8f325d0472447225af10e32ad3c15b8733
AmandaYi <891177434@qq.com>
2019-01-22 17:27:00 +0800

    webpack完结

--- webpack_demo/src/entery.js
+++
@@ -1,5 +0,0 @@
-
-import "./css/index.css"
-import "./less/index.less"
-document.getElementById("title").innerHTML = "Hello webpack!11呜呜呜呜11爱爱爱";
-
---
+++ webpack_demo/src/entry.js
@@ -0,0 +1,17 @@
+
+import "./css/index.css"
+import "./less/index.less"
+document.getElementById("title").innerHTML = "Hello webpack!11呜呜呜呜11爱爱爱";
+
+
+{
+    let a = 123456
+    let v= {v:2}
+    let c = v.v+a
+    console.log(c)
+    // console.log(a)
+}
+
+
+import $ from "jquery";
+$("#title").html("使用了jquery,改变了文字!");
