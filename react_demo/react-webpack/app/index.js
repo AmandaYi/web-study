fca221f8543c1e9fb055f1e009e2f96024d866f8
AmandaYi <891177434@qq.com>
2019-01-24 17:31:02 +0800

    r

--- react_demo/react-webpack/app/index.js
+++ react_demo/react-webpack/app/index.js
@@ -1,3 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
-ReactDOM.render(<div>Hello JS,React DOM</div>,document.getElementById("app"))
+
+
+
+
+
+
+import Person from "./person"
+import PersonA from "./persona"
+import PersonB from "./personb"
+
+import { Route, BrowserRouter as Router } from "react-router-dom"
+ReactDOM.render(
+
+    <Router>
+        <div>
+            <Route exact path="/" component={Person}></Route>
+            <Route path="/a" component={PersonA}></Route>
+            <Route path="/b" component={PersonB}></Route>
+        </div>
+    </Router>
+
+
+
+
+    , document.getElementById("app"))
