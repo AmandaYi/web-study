9d465d1bd2ae7613a575ff14c4b8b3ddee36963a
AmandaYi <891177434@qq.com>
2019-02-19 16:47:08 +0800

    多页面自动刷新配置

--- webpack4_demo/webpack.config.js
+++ webpack4_demo/webpack.config.js
@@ -2,15 +2,28 @@ const path = require("path")
 module.exports = {
     mode: "development",
     // 入口
-    entry: "./src/index.js",
+    entry: {
+        /* ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓  ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ */
+        // 这里新增加了一个入口,并修改了入口的名字也就是index,和 index2
+        index: "./src/index.js",
+        // 修改添加的地方, 这里添加了一个新的入口
+        index2: "./src/index2.js"
+        /* ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ */
+    },
     // 出口
     output: {
         // 出口的文件夹
         path: path.resolve(__dirname, "/dist/"),
         // 修改打包文件的名字,
         // [name] 的意思就是 根据入口文件的名字进行输出打包
-        filename: "build.js"
+        /* ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓  ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ */
+        // 修改添加的地方添加的地方, 这里修改了一下输出的文件名为入口的key值
+        filename: "[name].js",
+        publicPath: "public"
+        // PS, 强调, 如果这里有publicPath属性,并不会去覆盖掉devServer的属性,因为这是打包的环境
+        /* ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ */
     },
+
     // 模块,例如,解读CSS,图片如何转换,压缩
     module: {},
     // 插件,用于生产模板,和各项功能
@@ -19,18 +32,18 @@ module.exports = {
     devServer: {
         // 在这里增加了开发服务的功能
         // 设置需要监听的目录
-        contentBase:path.resolve(__dirname,'dist'),
+        contentBase: path.resolve(__dirname, 'dist'),
         //  
         host: "localhost",
         // 设置服务器的端口
         port: 3000,
         // 服务器是否启用压缩功能,一般启用
         compress: true,
- 
+
         // 实时刷新
         inline: true,
         // 新文件的内存路径与配置文件中的publicPath相关，如http://localhost:8080/{publicPath}/bundle.js
-        // publicPath: "/dist/"
+        publicPath: "/dist/"
     }
 }
 
