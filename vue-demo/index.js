d121ab34b35fe85b155000ae83e48ae7d878785f
AmandaYi <891177434@qq.com>
2019-02-26 22:11:53 +0800

    vue

---
+++ vue-demo/index.js
@@ -0,0 +1,18 @@
+
+let countryList = ["english", "zh", "india", "usa", "uk", "sing"]; // 字面量创建方式
+let bookId = [1,2,3,4,5,6,7]; // 字面量创建方式
+
+let arr = countryList.concat(bookId)
+// console.log(arr)
+let a =  arr.join("--")
+console.log(typeof a)
+console.log( Array.isArray(a))
+
+// console.log(countryList)
+// console.log(bookId)
+
+
+
+
+
+ 
