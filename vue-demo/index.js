903d8fdb9672cedc798f7bc46c34f3cf9ff4fa14
AmandaYi <891177434@qq.com>
2019-03-10 22:24:06 +0800

    hlsjs

--- vue-demo/index.js
+++ vue-demo/index.js
@@ -1,18 +1,33 @@
+// var arr = ["Tom", "Jerry", "Spike", "taike"]
+// // arr[0]
+// // arr[1]
+// // arr[2]
 
-let countryList = ["english", "zh", "india", "usa", "uk", "sing"]; // 字面量创建方式
-let bookId = [1,2,3,4,5,6,7]; // 字面量创建方式
+// // arr[x]
 
-let arr = countryList.concat(bookId)
-// console.log(arr)
-let a =  arr.join("--")
-console.log(typeof a)
-console.log( Array.isArray(a))
+// /**
+//  * 
+//  * 
+//  * for循环三步走
+//  * 1. 声明初始化值
+//  * 
+//  * 2. 创造终止条件, boolean
+//  * 
+//  * 3. 对初始化值进行改变,以至于能够打破终止条件
+//  *  */
 
-// console.log(countryList)
-// console.log(bookId)
+// // for (i = 0; i = i + 1; i < 3) {
+// //     //    console log;("hello"+var)
+// //     // console.log(arr)
+// // }
+// // 数组处理标准方式
+// for (let i = 0; i < arr.length; i++) {
+//     //    console log;("hello"+var)
+//     // console.log(arr)
+
+//     console.log("helleo " + arr[i])
+// }
+
+["Tom", "Jerry", "Spike", "taike"].sort((a, b) => { return a - b }).reverse().map(item => console.log("hello " + item))
 
 
-
-
-
- 
