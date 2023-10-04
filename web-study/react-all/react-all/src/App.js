3c23128ede72005a8642da123dd5bcb359947ffd
zhaozheyun <891177434@qq.com>
2023-10-05 00:58:16 +0800

    Hooks

--- web-study/react-all/react-all/src/App.js
+++ web-study/react-all/react-all/src/App.js
@@ -1,13 +1,16 @@
-import React  from 'react';
+import React from 'react';
 // import Index from './Components/BaseUse/index.jsx'
 // import Index from "./Components/advancedUse/index.jsx"
 // import Index from "./Components/ReduxUse/index"
-import Index from './Components/RouterUse';
+// import Index from './Components/RouterUse';
+// import Index from "./Components/SetStateCountDemo"
+import Index from "./Components/HooksDemo/index"
 class App extends React.Component {
   state = {
     count: 0
   }
   componentDidMount() {
+    // console.log(React.version)
   }
   render() {
     return (
@@ -17,3 +20,4 @@ class App extends React.Component {
 }
 
 export default App;
+
